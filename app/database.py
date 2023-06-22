dbe1b16cde6b7c6c7aa9990a0ae5e5d1c96c1321
dima <dimitori.g@gmail.com>
2023-06-22 19:34:34 +0900

    create simple CRUD app

---
+++ app/database.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_USER = os.environ.get('POSTGRES_USER', 'fastapi')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'password')
+POSTGRES_SERVER = os.environ.get('POSTGRES_SERVER', '127.0.0.1')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
+POSTGRES_DB = os.environ.get('POSTGRES_DB', 'fastapi')
+
+SQLALCHEMY_DATABASE_URL = "postgresql://{0}:{1}@{2}:{3}/{4}".format(
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    POSTGRES_SERVER,
+    POSTGRES_PORT,
+    POSTGRES_DB
+)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=True
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
