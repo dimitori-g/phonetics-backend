fe52028f2424614629deef187105fa61b772076b
dima <dimitori.g@gmail.com>
2023-06-29 10:44:30 +0900

    add pass encryption

--- app/crud.py
+++ app/crud.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, security
 
 
-def get_user(db: Session, user_id: int):
+def get_user_by_id(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
@@ -16,10 +16,10 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
+    hashed_password = security.encrypt_password(user.password)
     db_user = models.User(
         email=user.email,
-        hashed_password=fake_hashed_password
+        hashed_password=hashed_password
         )
     db.add(db_user)
     db.commit()
