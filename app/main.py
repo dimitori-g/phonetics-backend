e5bfb4fbab868bb3e969af50196ebc953210ba4b
dima <dimitori.g@gmail.com>
2023-06-29 10:46:53 +0900

    add login endpoint

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-from fastapi import Depends, FastAPI, HTTPException
+from typing import Annotated
+from datetime import timedelta
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+from . import crud, models, schemas, security
+from .database import engine
+from .services import get_db
 
 models.Base.metadata.create_all(bind=engine)
 
 
 tags_metadata = [
     {
-        "name": "users",
+        "name": "Users",
         "description": "Operations with users. The **login** logic is also here.",
     },
     {
-        "name": "items",
+        "name": "Items",
         "description": "Manage items. So _fancy_ they have their own docs.",
         "externalDocs": {
             "description": "Items external docs",
@@ -28,16 +32,7 @@ app = FastAPI(
     )
 
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/users/", tags=["users"], response_model=schemas.User)
+@app.post("/user/", tags=["Users"], response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -45,28 +40,54 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/", tags=["users"], response_model=list[schemas.User])
+@app.get("/users/", tags=["Users"], response_model=list[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}", tags=["users"], response_model=schemas.User)
+@app.get("/users/{user_id}", tags=["Users"], response_model=schemas.User)
 def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user_by_id(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
-@app.post("/users/{user_id}/items/", tags=["users"], response_model=schemas.Item)
+@app.post(
+        "/users/{user_id}/items/",
+        tags=["Users"],
+        response_model=schemas.Item
+)
 def create_item_for_user(
     user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_item(db=db, item=item, user_id=user_id)
 
 
-@app.get("/items/", tags=["items"], response_model=list[schemas.Item])
+@app.get("/items/", tags=["Items"], response_model=list[schemas.Item])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@app.post("/login", tags=["Users"], response_model=schemas.Token)
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db)
+):
+    user = crud.get_user_by_email(db, email=form_data.username)
+    if (not user
+            or not security.verify_password(
+            form_data.password, user.hashed_password
+            )):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=security.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = security.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
